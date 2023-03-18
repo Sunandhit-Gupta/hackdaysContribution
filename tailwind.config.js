d8215d77b3377d285515ee0ff842b32d99e8deea
NeozVBambah <neozvbambah@gmail.com>
2023-03-19 00:59:29 +0530

    built the basic layout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': ['myriad']
+      },
+      colors: {
+        'yellow':'#FFEA98',
+        'green':'#34A853',
+        'blue':'#909FE7',
+        'grey':'#F8F6F8',
+      }
+    },
+  },
+  plugins: [],
+}
